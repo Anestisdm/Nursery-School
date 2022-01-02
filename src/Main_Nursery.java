42472294cd2d0c1b65c0c52ee164146453afc4bf
Anestis Dimou <anestisdm77@gmail.com>
2022-01-02 17:25:13 +0200

    Add files via upload

---
+++ src/Main_Nursery.java
@@ -0,0 +1,101 @@
+package Nursery;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main_Nursery {
+
+	public static void main(String[] args) {
+		Create_Objects();//Construction of auxiliary objects for testing
+		//Scanner scanner = new Scanner(System.in);
+		String x = "1";//Auxiliary variable that helps us to terminate the application
+
+		while (x != "0") {
+			//Print Option menu with UI
+			ImageIcon icon = new ImageIcon("Nursery-School/images/Kindergarten.png");
+			Image image2 = icon.getImage().getScaledInstance(190,190,0);
+			x = (String)JOptionPane.showInputDialog(null, "\n~~~~~~~~~~~~~~~~ Options Menu ~~~~~~~~~~~~~~~~" +
+								"\n1.Print all students who have attended the school" +
+								"\n2.Register a new student in the current school year" +
+								"\n3.Delete student from the current school year" +
+								"\n4.Search school year data" +
+								"\n5.Search for classes taken by each teacher in the school" +
+								"\n6.Search for teacher data via social security number" +
+								"\n   Enter number [1-6] :   ","Menu",JOptionPane.INFORMATION_MESSAGE,new ImageIcon(image2),null,null);
+
+			if(x!=null) {
+				switch (x) {
+					case "0":
+						int a = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?");
+						if (a == JOptionPane.YES_OPTION) {
+							System.exit(0);
+						} else if (a == JOptionPane.NO_OPTION || a == JOptionPane.CANCEL_OPTION) {
+							continue;
+						}
+					case "1":
+						Student.Print_Students();
+						break;
+					case "2":
+						SchoolYear.Student_Registration();
+						break;
+					case "3":
+						SchoolYear.Student_Unregistration();
+						break;
+					case "4":
+						SchoolYear.SchoolYear_Data();
+						break;
+					case "5":
+						Teacher.Teacher_Classes();
+						break;
+					case "6":
+						Teacher.AMKA_Index();
+						break;
+					default:
+						JOptionPane.showMessageDialog(null, "Wrong Input!", "Error",JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			else{
+				int a = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?","Exit",JOptionPane.WARNING_MESSAGE);
+				if (a == JOptionPane.YES_OPTION) {
+					System.exit(0);
+				} else if (a == JOptionPane.NO_OPTION || a == JOptionPane.CANCEL_OPTION) {
+					continue;
+				}
+			}
+		}
+	}
+	//Method of creating objects
+	private static void Create_Objects() {
+		Student s1_18= new Student ("Vassilios Papadopoulos","18/03/2015");
+		Student s2_18= new Student ("Eleftheria Grigoriou", "03/06/2015");
+		Student s3_18= new Student ("Konstantina Anastasaki","03/12/2015");
+		Student s4_18= new Student ("Nikolaos Mavropoulos","14/05/2015");
+		Student s5_18= new Student ("George Antonopoulos","03/09/2015");
+		Student s6_18= new Student ("Efstathios Demetriou","11/02/2014");
+		Student s7_18= new Student ("Theodoros Vassilakopoulos","27/04/2014");
+		Student s8_18= new Student ("Angelina Karakasi","18/01/2014");
+		Student s9_18= new Student ("Eleni Kalogianni", "22/11/2014");
+		Student s10_18= new Student ("Georgia Kehagia","09/08/2014");
+		Teacher t1=new Teacher("Maria Giannellou","01058219503");
+		Teacher t2=new Teacher("Konstantina Zafeiropoulou","17077732902");
+		Student[] st1_18 = {s1_18,s2_18,s3_18,s4_18,s5_18,null,null,null,null,null};
+		Student[] st2_18 = {s6_18,s7_18,s8_18,s9_18,s10_18,null,null,null,null,null};
+		SchoolYear y18 = new SchoolYear ("2018-2019",st1_18,t1,st2_18,t2);
+
+			Student s1_19 = new Student("Alexandros Vafiadis", "30/06/2016");
+			Student s2_19 = new Student("Irini Alexopoulou", "24/03/2016");
+			Student s3_19 = new Student("Dimitra Giannakou", "07/02/2016");
+			Student s4_19 = new Student("Charalampos Kalogeras", "28/11/2016");
+			Student s5_19 = new Student("Vasiliki Eleftheriadou", "11/08/2016");
+			Student s6_19 = new Student("Nikolaos Vouroò", "27/09/2015");
+			Student s7_19 = new Student("Ilias Galanopoulos", "04/06/2015");
+			Student s8_19 = new Student("Athena Deli", "11/01/2015");
+			Student s9_19 = new Student("Stella Ioannidou", "24/09/2015");
+			Student s10_19 = new Student("Marianna Komninou", "18/04/2015");
+			Teacher t3 = new Teacher("Nikoletta Androutsou", "21088432605");
+			Student[] st1_19 = {s1_19, s2_19, s3_19, s4_19, s5_19, null, null, null, null, null, null, null, null, null, null};
+			Student[] st2_19 = {s6_19, s7_19, s8_19, s9_19, s10_19, null, null, null, null, null, null, null, null, null, null};
+			SchoolYear y19 = new SchoolYear("2019-2020", st1_19, t1, st2_19, t3);
+		}
+	}
---
+++ src/Teacher.java
@@ -0,0 +1,85 @@
+package Nursery;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Teacher {
+
+private String id;//Unique teacher id
+private String name;//Teacher's name
+private String AMKA;//Social Security Number of Teacher
+private static Teacher[] Teachers = new Teacher[100];//Static table containing all Teacher type objects
+private static int count = 0;//Teacher object counter where it is used to store objects in the static table but also to run through the specific table and avoid the NullPointerException exception
+
+
+//Constructor of Teacher class
+public Teacher(String name, String AMKA) {
+	Teachers[count] = this;//Storing objects in the Teachers static table
+	count++;		
+	this.id = "T"+SchoolYear.gety()+"00"+count;//Construction of Student type objects of the T18001 format
+	this.name = name;
+	this.AMKA = AMKA;
+}
+
+//Method where it returns the instance variable name
+public String getName() {
+	return name;
+}
+
+
+//Method where it prints in detail which classes each teacher has undertaken
+public static void Teacher_Classes() {
+	String Classes[] = new String[Teachers.length-1] ;
+
+	//We run the table the static table Teachers
+	for (int i=0;i<count;i++) {
+		//Print the name of each teacher
+		Classes[i] = "\nHis / her classes "+ Teachers[i].name+" are the folowing:\n";
+		//We go through the static table SchoolYears and check if in that year is the name of the teacher and then we print the school year as well as the class where he had taken
+		for (int x=0;x<SchoolYear.getCount();x++) { 
+			if (Teachers[i].name.equals(SchoolYear.getSchoolYears()[x].getObject1().name)) {
+				Classes[i] += "- The year "+SchoolYear.getSchoolYears()[x].getYear()+" the PreKindergarten class\n";
+			}
+			else if (Teachers[i].name.equals(SchoolYear.getSchoolYears()[x].getObject2().name)) {
+				Classes[i] +="- The year "+SchoolYear.getSchoolYears()[x].getYear()+" the Kindergarten class\n";
+
+			}
+		}
+	}
+	String Returntxt = "";
+	for (String c: Classes){
+		if (c==null){
+			break;
+		}
+		Returntxt+=c;
+	}
+	JOptionPane.showMessageDialog(null,"TEACHER CLASSES\n"+Returntxt,"Classes",JOptionPane.INFORMATION_MESSAGE);
+}
+
+//Method where it searches for teacher data with the Social Security Number where we enter it and then displays it
+public static void AMKA_Index() {
+	boolean x=false;//Auxiliary variable where it tells us if the Social Security Number entered by the user exists in memory
+	while (x==false) {
+		String input = (String)JOptionPane.showInputDialog(null,"Please enter the teacher's Social Security Number: \n [eg 17077732902]");
+		//We give the user the option to press 0 and end the search
+		if (input==null) {
+			x=true;//We stop the loop because the user entered 0
+		}
+		else {
+			//We go through the static table Teachers
+			for (int i=0;i<count;i++) { 
+				if (Teachers[i].AMKA.equals(input)) {
+					//If we find an object of type Teacher with Social Security Number (instance variable) where it is the same as the input we display the data of the specific teacher
+					JOptionPane.showMessageDialog(null,"\nThe details of the teacher with Social Security Number '"+input+"' are the following:\nId: "+Teachers[i].id+"\nFull name: "+Teachers[i].name);
+					x=true;//We stop the loop because the teacher was found
+				}
+			}
+		}
+		if (x==false) {
+			//We display an appropriate message as we know that no Teacher object was found with Social Security Number (instance variable) where it is the same as the user input
+			JOptionPane.showMessageDialog(null,"The Social Security Number you entered does not correspond to a teacher","Error",JOptionPane.ERROR_MESSAGE);
+		}	
+	}
+}
+
+}
